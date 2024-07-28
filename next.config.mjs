5d540fb2ba1d7e8e95e2eb5b34b60524f090db1d
contigen <69436315+contigen@users.noreply.github.com>
2024-07-28 20:33:34 +0100

    Add a file loader for .mp3 files

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
 
-export default nextConfig;
+const nextConfig = {
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.(mp3)$/,
+      use: {
+        loader: 'file-loader',
+        options: {
+          publicPath: '/_next/static/sounds/',
+          outputPath: 'static/sounds/',
+          name: '[name].[ext]',
+          esModule: false,
+        },
+      },
+    })
+    return config
+  },
+}
+
+export default nextConfig
