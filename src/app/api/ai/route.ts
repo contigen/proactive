15ca8833d0e3bf000d7ac978f4534b88f1fdf018
contigen <69436315+contigen@users.noreply.github.com>
2024-07-28 23:59:42 +0100

    Update app

--- src/app/api/ai/route.ts
+++ src/app/api/ai/route.ts
@@ -1,6 +1,50 @@
 import { GoogleGenerativeAI } from '@google/generative-ai'
 import { GoogleGenerativeAIStream, StreamingTextResponse } from 'ai'
 
+const INSTRUCTION = `Transform user speech transcripts into structured to-do list items, with details formatted for ease of use and interactivity. Use the Contact Picker API to assist in selecting contacts for inline links where applicable. Ensure the link text displays the contact's name, with the link itself being the value of href.
+
+Instructions:
+
+Task Identification:
+
+Identify the main action and subject in each transcript to determine the task. Look for keywords indicating actions, such as "send," "meet," "call," "buy," "attend," etc.
+Action Type Detection:
+
+Categorize each task by its type:
+"Mail" for emails
+"Call" for phone calls
+"Meeting" for scheduled meetings
+"SMS" for text messages
+"Purchase" for buying items
+"Visit" for places to go
+Contact and Links:
+
+Utilize the Contact Picker API to select contacts and create appropriate interactive links based on the action type. Ensure the link text displays the contact's name.
+Mail: Use mailto: with the contact's email address.
+Example: "Send a mail to Joe" -> <a href="mailto:joe@example.com">Joe</a>
+SMS: Use sms: with the contact's phone number.
+Example: "Send an SMS to Dad" -> <a href="sms:1234567890">Dad</a>
+Call: Use tel: with the contact's phone number.
+Example: "Call Mom" -> <a href="tel:0987654321">Mom</a>
+Time and Scheduling:
+
+Extract any time information and convert it into a 12-hour format (e.g., "10 PM").
+Order tasks chronologically based on time when available.
+Location Recognition:
+
+Detect locations and set them as the task location if specified (e.g., "Meet at Starbucks").
+Task Completion:
+
+Set the completed status to false by default.
+Null Values:
+
+Set unspecified details (such as time or location) to null.
+------
+
+This is the prompt:
+
+`
+
 export async function POST(req: Request) {
   const { prompt } = await req.json()
   const apiKey = process.env.GOOGLE_GENERATIVE_AI_API_KEY
@@ -8,23 +52,25 @@ export async function POST(req: Request) {
   const genAI = new GoogleGenerativeAI(apiKey)
   const model = genAI.getGenerativeModel({
     model: `gemini-1.5-flash-latest`,
-    systemInstruction: {
-      role: `assistant`,
-      parts: [
-        {
-          text: `create a to-do list for me based on my input and return in JSON format that can be parsed by JavaScript. Use the following TypeScript type to mock the structure; details not specified should be null, and return only JSON data in an array, ensure the time is in a 12-hour format and pass data only determined from the prompt,also re-order tasks based on time specified, and no hallucinations;  type Activity = {
-            completed:boolean
-            activity: string
-            activityType: string
-            location: string
-            contact: string
-            time: string
-          }[]`,
-        },
-      ],
-    },
+    // systemInstruction: {
+    //   role: `assistant`,
+    //   parts: [
+    //     {
+    //       text: INSTRUCTION,
+    //     },
+    //   ],
+    // },
   })
-  const result = await model.generateContentStream(prompt)
+  const result = await model.generateContentStream(INSTRUCTION + prompt)
   const stream = GoogleGenerativeAIStream(result)
   return new StreamingTextResponse(stream)
 }
+
+// create a to-do list for me based on my input and return in JSON format that can be parsed by JavaScript. Use the following TypeScript type to mock the structure; details not specified should be null, and return only JSON data in an array, ensure the time is in a 12-hour format and pass data only determined from the prompt,also re-order tasks based on time specified, and no hallucinations;  type Activity = {
+//   completed:boolean
+//   activity: string
+//   activityType: string
+//   location: string
+//   contact: string
+//   time: string
+// }[]
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,37 @@
+import { ActivityInterface } from '&/features/activity/activity-interface'
+
 export default function Page() {
   return (
     <div className='mb-10'>
-      <div className='bg-blue-500 px-20 py-10 text-white rounded-3xl mx-20 my-14 flex items-center justify-evenly'>
+      <div className='bg-blue-500 px-20 py-10 text-white rounded-3xl mx-8 md:mx-20 my-14 flex items-center justify-evenly flex-wrap lg:flex-nowrap gap-10'>
         <h1>ProActive.</h1>
-        <svg
-          xmlns='http://www.w3.org/2000/svg'
-          width='128'
-          height='128'
-          viewBox='0 0 24 24'
-          fill='none'
-        >
-          <path
-            d='M8 2v3M16 2v3M7 11h8M7 15h5M15 22H9c-5 0-6-2.06-6-6.18V9.65c0-4.7 1.67-5.96 5-6.15h8c3.33.18 5 1.45 5 6.15V16'
-            stroke='#ffffff'
-            stroke-width='1.5'
-            stroke-miterlimit='10'
-            stroke-linecap='round'
-            stroke-linejoin='round'
-          ></path>
-          <path
-            d='m21 16-6 6v-3c0-2 1-3 3-3h3Z'
-            stroke='#ffffff'
-            stroke-idth='1.5'
-            stroke-linecap='round'
-            stroke-linejoin='round'
-          ></path>
-        </svg>
+        <div>
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            width='128'
+            height='128'
+            viewBox='0 0 24 24'
+            fill='none'
+          >
+            <path
+              d='M8 2v3M16 2v3M7 11h8M7 15h5M15 22H9c-5 0-6-2.06-6-6.18V9.65c0-4.7 1.67-5.96 5-6.15h8c3.33.18 5 1.45 5 6.15V16'
+              stroke='#ffffff'
+              stroke-width='1.5'
+              stroke-miterlimit='10'
+              stroke-linecap='round'
+              stroke-linejoin='round'
+            ></path>
+            <path
+              d='m21 16-6 6v-3c0-2 1-3 3-3h3Z'
+              stroke='#ffffff'
+              stroke-idth='1.5'
+              stroke-linecap='round'
+              stroke-linejoin='round'
+            ></path>
+          </svg>
+        </div>
       </div>
-      <p className='text-3xl font-[550] tracking-tight w-[50ch] my-8 mx-20'>
+      <p className='text-2xl md:text-3xl font-[550] tracking-tight w-[50ch] my-8 mx-8 md:mx-20 max-w-full'>
         Avoid the mental inertia. Skip the mental gymnastics.
         <br />
         Just{' '}
@@ -35,11 +39,13 @@ export default function Page() {
           say
         </span>{' '}
         whatever you want to do. Get a Transcript, <br />
-        get ready with your tasks set with AI, to kickoff your day & stay{' '}
+        get ready with your tasks set with AI, to kickoff your day & stay
+        <wbr />{' '}
         <span className='bubble right bg-gray-600 text-white px-2 pb-px'>
           active.
         </span>{' '}
       </p>
+      <ActivityInterface />
     </div>
   )
 }
